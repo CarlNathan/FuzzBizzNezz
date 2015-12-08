ae74cd88a186bbd1389429f4f941845d45346171
CarlNathan <c.n.udren@gmail.com>
2015-12-07 23:55:08 -0500

    Completed FuzzBizzNezz assignment as described.  Added ReadMe.

--- FuzzBizzNezz/main.c
+++ FuzzBizzNezz/main.c
@@ -2,14 +2,43 @@
 //  main.c
 //  FuzzBizzNezz
 //
-//  Created by Carl Udren on 12/7/15.
-//  Copyright © 2015 Carl Udren. All rights reserved.
+//  Created by Carl Nate Udren on 12/7/15.
+//  Copyright © 2015 Carl Nate Udren. All rights reserved.
 //
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int nezz, fuzz, bizz; //initializes variables for user input.
+    printf("Enter a number value for Nezz\n");
+    scanf("%d", &nezz); //assumes user enters an interger.  No input verification.
+    printf("Enter a number value for Bizz\n");
+    scanf("%d", &bizz); // assumes user enters an interger.  No input verification.
+    printf("Enter a number value for Fuzz\n");
+    scanf("%d", &fuzz); //assumes user enters an interger.  No input verification.
+    
+    
+    int x = 0; //initializes counter used in for loop.
+    for (x = 1; x <= nezz; x++)
+    {
+        
+        if (x % fuzz != 0 && x % bizz != 0)
+        {
+            printf("%d", x);
+        }
+        
+        if (x % fuzz == 0)
+        {
+            printf("Fuzz");
+        }
+        
+        if (x % bizz == 0)
+        {
+            printf("Bizz");
+        }
+        
+        printf("\n");
+    }
     return 0;
 }
